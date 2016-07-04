9c159ca5033eafd95cdc959b6b06f7149c7029e7
Akshay Iyer <akshay.iyer@nyu.edu>
2016-07-03 20:17:11 -0400

    Created the core data model

---
+++ Image.swift
@@ -0,0 +1,17 @@
+//
+//  Image.swift
+//  VirtualTourist
+//
+//  Created by Akshay Iyer on 7/3/16.
+//  Copyright © 2016 akshaytiyer. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Image: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
---
+++ Pin.swift
@@ -0,0 +1,17 @@
+//
+//  Pin.swift
+//  VirtualTourist
+//
+//  Created by Akshay Iyer on 7/3/16.
+//  Copyright © 2016 akshaytiyer. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Pin: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
